bc458eebbd36d9cf31c429ca00357ad8c2384647
jerryxhe <rebound1618@gmail.com>
2017-04-30 23:44:38 -0400

    Update archive_video_link.py

--- archive_video_link.py
+++ archive_video_link.py
@@ -1,6 +1,7 @@
 # @author Jerry He
 # @desc this script works in the Pythonista app in iOS
 # @purpose gets a video link from clipboard and inserts into a database
+__author__ = 'jerryxhe'
 
 import clipboard
 #from mysql import mysqlClient
