b0ba70b5b797b0ac28eb781fc1cfbf994161ca45
jerryxhe <rebound1618@gmail.com>
2017-04-30 23:42:51 -0400

    Create archive_video_link.py

---
+++ archive_video_link.py
@@ -0,0 +1,15 @@
+# @author Jerry He
+# @desc this script works in the Pythonista app in iOS
+# @purpose gets a video link from clipboard and inserts into a database
+
+import clipboard
+#from mysql import mysqlClient
+from psql import psqlClient
+
+link = clipboard.get()
+
+print link
+
+#m = mysqlClient()
+m = psqlClient()
+m.execute("INSERT INTO eduvideo(source, created_on) VALUES ('%s', NOW())" % link)
